765223934f466e1ed41deded3a0e6a015fa24067
valtteri.yli-olli <ab5818@jamk.student.fi>
2023-08-30 14:09:37 +0300

    AddedFriendToScene

--- Assets/Scripts/FoxController.cs
+++ Assets/Scripts/FoxController.cs
@@ -7,7 +7,7 @@ public class FoxController : MonoBehaviour
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    private float playerSpeed = 2.0f;
+    private float playerSpeed = 6.0f;
     private float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
 
