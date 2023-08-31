1cd3fe24c6d5a30e18a1d688288e665399b87cfc
Zsomczie <zsomi.f@gmail.com>
2023-08-31 15:54:01 +0300

    Rabbit dialogue

---
+++ Assets/Scripts/Interaction.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    [SerializeField] bool isActive = false;
+    [SerializeField] bool used = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)&&isActive)
+        {
+            used = true;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        isActive = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        isActive = false;
+    }
+}
